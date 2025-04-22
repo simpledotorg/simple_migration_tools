466ee5782dc8035222c58921dbc267fa90e38c15
Arnaud DEMARCQ <ademarcq@RTSL-N7VH6K2VP9.local>
2025-04-22 17:21:46 +0200

    Adds do_dump.sh

--- state_migration_dumps/create_dump_schema.sql
+++ state_migration_dumps/create_dump_schema.sql
@@ -2,7 +2,7 @@ drop schema if exists simple_dump_data cascade;
 create schema simple_dump_data;
 
 --
--- facilitie's
+-- facilities
 --
 CREATE TABLE simple_dump_data.facilities as 
 select * from public.facilities where state= 'West Bengal';
@@ -19,9 +19,37 @@ where assigned_facility_id in (select id from simple_dump_data.facilities);
 --
 CREATE TABLE simple_dump_data.appointments as 
 SELECT * FROM public.appointments
-where patient_id in (select id from simple_dump_data.patients);
+where patient_id in (select id from simple_dump_data.patients)
+and facility_id in (select id from simple_dump_data.facilities); -- Doing both to ensure FK will be satisfied
+
+--
+-- addresses 
+--
+CREATE TABLE simple_dump_data.addresses as 
+SELECT * FROM public.addresses
+where id in (select address_id from simple_dump_data.patients);
+
+--
+-- blood_pressures
+--
+CREATE TABLE simple_dump_data.blood_pressures as 
+SELECT * FROM public.blood_pressures
+where patient_id in (select id from simple_dump_data.patients)
+and facility_id in (select id from simple_dump_data.facilities); -- Doing both to ensure FK will be satisfied
+
+--
+-- blood_sugars
+--
+CREATE TABLE simple_dump_data.blood_sugars as 
+SELECT * FROM public.blood_sugars
+where patient_id in (select id from simple_dump_data.patients)
+and facility_id in (select id from simple_dump_data.facilities); -- Doing both to ensure FK will be satisfied
+
+--
+-- protocol_drugs
+-- 
+CREATE TABLE simple_dump_data.protocol_drugs as 
+SELECT * FROM public.protocol_drugs
 
 
 
--- select count(*) from simple_dump_data.patients;
--- select count(*) from public.patients;
---
+++ state_migration_dumps/do_dump.sh
@@ -0,0 +1,25 @@
+##
+## Creates the dump schema
+##
+echo todo
+
+##
+## Gets the dump from the dump schema
+## 
+pg_dump -U postgres -h localhost -d simple-server_test --schema=simple_dump_data -f ./target/test_dump.sql
+
+##
+## Zips the resulting file
+##
+zip ./target/test_dump.zip ./target/test_dump.sql
+
+##
+## todo: put it into S3
+##
+echo awscli ...
+
+##
+## Cleans everythings
+##
+echo rm -rf ./target
+
